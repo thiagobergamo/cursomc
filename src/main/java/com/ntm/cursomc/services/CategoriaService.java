377d1c8241488be95561db4e3c0356eae364ae64
Thiago Bergamo <thiagobergamo@gmail.com>
2017-12-10 15:55:12 -0200

    Adicionado tratamento de exceptions

--- src/main/java/com/ntm/cursomc/services/CategoriaService.java
+++ src/main/java/com/ntm/cursomc/services/CategoriaService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.ntm.cursomc.domain.Categoria;
 import com.ntm.cursomc.repositories.CategoriaRepository;
+import com.ntm.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -14,6 +15,11 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer  id) {
 		Categoria obj = repo.findOne(id);
+		
+		if (obj == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado! Id: " + id
+					+ ", Tipo: " + Categoria.class.getName());
+		}
 		return obj;
 	}
 }
