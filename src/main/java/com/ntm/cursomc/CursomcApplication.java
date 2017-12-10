b54afa9ed56737508a9ecc6e810fe271c4e50415
Thiago Bergamo <thiagobergamo@gmail.com>
2017-12-10 17:55:48 -0200

    implementado domain e repository para cidades e estados

--- src/main/java/com/ntm/cursomc/CursomcApplication.java
+++ src/main/java/com/ntm/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ntm.cursomc.domain.Categoria;
+import com.ntm.cursomc.domain.Cidade;
+import com.ntm.cursomc.domain.Estado;
 import com.ntm.cursomc.domain.Produto;
 import com.ntm.cursomc.repositories.CategoriaRepository;
+import com.ntm.cursomc.repositories.CidadeRepository;
+import com.ntm.cursomc.repositories.EstadoRepository;
 import com.ntm.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class CursomcApplication implements CommandLineRunner{
 	CategoriaRepository categoriaRepository;
 	@Autowired
 	ProdutoRepository produtoRepository;
+	@Autowired
+	EstadoRepository estadoRepository;
+	@Autowired
+	CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -40,7 +48,20 @@ public class CursomcApplication implements CommandLineRunner{
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
 		categoriaRepository.save(Arrays.asList(cat1, cat2));
 		produtoRepository.save(Arrays.asList(p1, p2, p3));
+		estadoRepository.save(Arrays.asList(est1, est2));
+		cidadeRepository.save(Arrays.asList(c1, c2, c3));
+		
 	}
 }
